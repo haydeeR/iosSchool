e274c2246d1df3160fb4160c8ae428b51b17ca6b
haydeerodriguez <hrodriguez@nearsoft.com>
2019-09-28 02:54:32 -0500

    Fixed Configuration UIGraphicsImage and render

--- drag&DropEditor/drag&DropEditor/Controllers/ViewController.swift
+++ drag&DropEditor/drag&DropEditor/Controllers/ViewController.swift
@@ -13,11 +13,19 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var colorCollectionView: UICollectionView!
     
     var colors = [UIColor]()
+    var image: UIImage?
+    var topFontColor = UIColor.white
+    var bottomFontColor = UIColor.white
+    var topText = "Welcome to iOS School"
+    var bottomText = "Welcome to the magic! "
+    var topFontName = "Avenir Next"
+    var bottomFontName = "Baskerville"
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         setupColors()
+        renderEditor()
     }
     
     private func setupColors() {
@@ -45,5 +53,38 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return cell
     }
     
+    private func renderEditor(){
+        let mainRect = CGRect(x: 0, y: 0, width: 3000, height: 2400)
+        let topRect = CGRect(x: 300, y: 200, width: 2400, height: 800)
+        let bottomRect = CGRect(x: 300, y: 1800, width: 2400, height: 600)
+        
+        let topFont = UIFont(name: topFontName, size: 250) ?? UIFont.systemFont(ofSize: 250)
+        let bottomFont = UIFont(name: bottomFontName, size: 120) ?? UIFont.systemFont(ofSize: 120)
+        
+        let centered = NSMutableParagraphStyle()
+        centered.alignment = .center
+        let topAttributes: [NSAttributedString.Key : Any] = [
+            .foregroundColor: topFontColor,
+            .font: topFont,
+            .paragraphStyle: centered
+        ]
+        let bottomAttributes: [NSAttributedString.Key : Any] = [
+            .foregroundColor: bottomFontColor,
+            .font: bottomFont,
+            .paragraphStyle: centered
+        ]
+        
+        let render = UIGraphicsImageRenderer(size: mainRect.size)
+        editorImage.image = render.image(actions: { (context) in
+            UIColor.gray.set()
+            context.fill(mainRect)
+            
+            image?.draw(at: CGPoint(x: 0, y: 0))
+            self.topText.draw(in: topRect, withAttributes: topAttributes)
+            self.bottomText.draw(in: bottomRect, withAttributes: bottomAttributes)
+        })
+        
+        
+    }
 }
 
