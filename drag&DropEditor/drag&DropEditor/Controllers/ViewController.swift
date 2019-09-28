ec7f698cd957b6973b7de31d8c9c05d1489b6249
haydeerodriguez <hrodriguez@nearsoft.com>
2019-09-28 01:44:01 -0500

    TableViewController, UIColor, UIFont, CollectionViewController

--- drag&DropEditor/drag&DropEditor/Controllers/ViewController.swift
+++ drag&DropEditor/drag&DropEditor/Controllers/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var editorImage: UIImageView!
+    @IBOutlet weak var colorCollectionView: UICollectionView!
+    
+    var colors = [UIColor]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        setupColors()
+    }
+    
+    private func setupColors() {
+        colors += [.black, .blue,.orange, .purple, .green, .gray, .red, .magenta]
+        
+        for hue in 0...9{
+            for sat in 1...10{
+                let color = UIColor(hue: CGFloat(hue)/10, saturation: CGFloat(sat)/10, brightness: 1, alpha: 1)
+                self.colors.append(color)
+            }
+        }
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath)
+        let color = colors[indexPath.row]
+        cell.backgroundColor = color
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 5
+        
+        return cell
+    }
+    
 }
 
