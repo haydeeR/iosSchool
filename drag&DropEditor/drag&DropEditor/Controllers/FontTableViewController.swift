f89357e69783bd444505f783cc30f939142fae5d
haydeerodriguez <hrodriguez@nearsoft.com>
2019-09-28 01:27:08 -0500

    Solved first challenge

--- drag&DropEditor/drag&DropEditor/Controllers/FontTableViewController.swift
+++ drag&DropEditor/drag&DropEditor/Controllers/FontTableViewController.swift
@@ -10,37 +10,32 @@ import UIKit
 
 class FontTableViewController: UITableViewController {
 
+    let fonts = UIFont.familyNames.sorted()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return fonts.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "fontCell", for: indexPath)
+        let fontName = fonts[indexPath.row]
+        cell.textLabel?.text = fontName
+        cell.textLabel?.font = UIFont(name: fontName, size: 17)
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
